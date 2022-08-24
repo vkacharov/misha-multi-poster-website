cfd0a2d40fdbfd509aa660738a4e046898a92b71
vkacharov <velizar.kacharov@gmail.com>
2022-08-24 13:48:12 +0200

    Load the FB script synchronously to avoid race conditions during page load

--- src/main.js
+++ src/main.js
@@ -5,25 +5,8 @@ import Notifications from '@kyvg/vue3-notification'
 import './style.css'
 import App from './App.vue'
 
-function initFbSdk() {
-    return new Promise(resolve => {
-        window.fbAsyncInit = function() {
-            FB.init({
-              appId            : `${import.meta.env.VITE_APPID}`,
-              autoLogAppEvents : true,
-              xfbml            : true,
-              version          : 'v14.0'
-            });
-        }
-
-        resolve();
-    });
-}
-
-initFbSdk().then(() => {
-    const pinia = createPinia();
-    const app = createApp(App);
-    app.use(pinia);
-    app.use(Notifications);
-    app.mount('#app');
-});
+const pinia = createPinia();
+const app = createApp(App);
+app.use(pinia);
+app.use(Notifications);
+app.mount('#app');
