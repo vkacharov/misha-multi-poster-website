33ec6de554d32c859959355124580f1867a7d187
vkacharov <velizar.kacharov@gmail.com>
2022-08-29 23:59:21 +0200

    Revert "Make the FB application id an environment variable"
    This reverts commit 31d3ce3dfbb11542b01e9573736145eb5669a752.

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import mkcert from'vite-plugin-mkcert'
-import VitePluginHtmlEnv from 'vite-plugin-html-env'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     https: true
   },
-  plugins: [vue(), mkcert(), VitePluginHtmlEnv()]
+  plugins: [vue(), mkcert()]
 })
