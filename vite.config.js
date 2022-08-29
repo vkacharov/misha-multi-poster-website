31d3ce3dfbb11542b01e9573736145eb5669a752
vkacharov <velizar.kacharov@gmail.com>
2022-08-29 23:28:46 +0200

    Make the FB application id an environment variable

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import mkcert from'vite-plugin-mkcert'
+import VitePluginHtmlEnv from 'vite-plugin-html-env'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     https: true
   },
-  plugins: [vue(), mkcert()]
+  plugins: [vue(), mkcert(), VitePluginHtmlEnv()]
 })
